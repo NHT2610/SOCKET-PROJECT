15a4da3b4f28fd031bf96be4e6957b8cc4684a1a
Nguyen Huu Truc <nguyenhuutruc26102002@gmail.com>
2021-12-10 20:01:12 +0700

    last commit

--- Client/Client/ClientDlg.h
+++ Client/Client/ClientDlg.h
@@ -27,11 +27,8 @@ protected:
 
 	// Generated message map functions
 	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
-public:
-	afx_msg void OnEnChangeEdit1();
-	afx_msg void OnBnClickedOk();
-	afx_msg void OnBnClickedCancel();
 };
